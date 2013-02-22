1a7c995a0f884c9dae9a22a8d6d1b802f91fe73e
Alexander Couzens <lynxis@c-base.org>
2013-02-22 14:43:33 +0100

    added getopt + fprintf(stderr) + udp(working)

--- src/main.c
+++ src/main.c
@@ -11,63 +11,103 @@
 #include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <string.h>
-
+#include <unistd.h>
 #include <sys/epoll.h>
 
 #include "udp.h"
 
 
 #define MAX_EVENTS 16
-#define PCAP_CHECK_ERROR(expression, ...) do { \
-    ret = expression; \
-    if(ret == NULL) { \
-        fprintf("Error %s", "I"); \
-    } \
-    } while(0)
 
 void handle_pcap(pcap_t *handle) {
     struct pcap_pkthdr pkghdr;
-    const u_char *rawpacket;
-    while((rawpacket = pcap_next(handle, &pkghdr)) != NULL) {
-        send_udp(&pkghdr, rawpacket);
+    const u_char *rawpacket = pcap_next(handle, &pkghdr);
+    int ret = 0;
+    while (rawpacket != NULL) {
+        ret = send_udp(&pkghdr, rawpacket);
+        if(ret <0) {
+            fprintf(stderr, "error while sending udp packet\n");
+        }
         printf("pkglen %i - pkgcap %i\n", pkghdr.len, pkghdr.caplen);
+        rawpacket = pcap_next(handle, &pkghdr);
     }
 }
 
+void usage(char *progname, char *filter) {
+    fprintf(stderr,
+            "%s [-i dev][-P port] -T ip [filter]\n"
+            "\n"
+            "most options are also found in tcpdump - see man tcpdump for further help."
+            "-i dev - set capture device - default any\n"
+            "-T ip  - set target ip\n"
+            "-P port - set target port - default 3999\n"
+            "-s snaplen - set snaplen\n"
+            "-B buffersize\n"
+            "filter - default filter is %s\n"
+     , progname, filter);
+    exit(1);
+}
 
 /**
 - repr(tcpdump -i any udp port 53 or icmp or arp)
-- udp writeto(192.168.2.1:9986, packet)
-- eloop()
  */
-int main(int argv, char *argc[]) {
-    char dev[] = "wlan1";
+int main(int argc, char *argv[]) {
+    char default_filter[] = "(udp port 53) or icmp or arp";
+    char default_device[] = "any";
+    int snaplen = 64 * 1024;
+    int read_timeout_ms = 1;
+    int buffersize = 0;
+    int port = 3999;
+
+    char *ip = NULL;
+    char *dev = NULL;
+    char *filter = NULL;
+
     char errbuf[PCAP_ERRBUF_SIZE];
     int ret;
-    int snaplen = 64 * 1024;
-    int read_timeout_ms = 1000;
     int file_handle;
     pcap_t *handle;
     struct bpf_program compiled_filter;
     bpf_u_int32 mask;
-    bpf_u_int32 netip;
-    char filter[] = "(udp port 53) or icmp or arp";
 
     struct epoll_event events[MAX_EVENTS];
-    char ip[] = "192.168.1.1";
-    int port = 3999;
 
-//    dev = pcap_lookupdev(given_dev);
-//    if(dev == NULL) {
-//        printf("pcap lookupdev failed: %s\n", errbuf);
-//        exit(1);
-//    }
+    char ch;
 
-    ret = pcap_lookupnet(dev, &netip, &mask, errbuf);
-    if(dev == NULL) {
-        printf("pcap lookupnet failed: %s\n", errbuf);
-        exit(1);
+    while ((ch = getopt(argc, argv, "s:T:P:i:B:")) != -1) {
+        switch (ch) {
+        case 's':
+            snaplen = atoi(optarg);
+            break;
+        case 'T':
+            ip = calloc(strlen(optarg), 1);
+            strncpy(ip, optarg, strlen(optarg));
+            break;
+        case 'P':
+            port = atoi(optarg);
+            break;
+        case 'i':
+            dev = calloc(strlen(optarg) + 1, 1);
+            strncpy(dev, optarg, strlen(optarg));
+            break;
+        default:
+            usage(argv[0], default_filter);
+        }
     }
+    if(optind >= argc) {
+        filter = default_filter;
+    } else {
+        // TODO: implement filter option
+        //    printf("name argument = %s\n", argv[optind]);
+    }
+    if(ip == NULL) {
+        usage(argv[0], default_filter);
+    }
+    if(dev == NULL) {
+        dev = default_device;
+    }
+
+    // init pcap stuff
     printf("Using dev %s\n", dev);
     int prmisc = 1;
     handle =  pcap_create(dev, errbuf);
@@ -78,67 +118,73 @@ int main(int argv, char *argc[]) {
 
     pcap_set_promisc(handle, prmisc);
     pcap_set_snaplen(handle, snaplen);
-    pcap_set_timeout(handle, 1);
+    pcap_set_timeout(handle, read_timeout_ms);
     pcap_setnonblock(handle, 1, errbuf);
-//    pcap_set_buffer_size(handle, 16 * 1024);
+    if(buffersize > 0) {
+        pcap_set_buffer_size(handle, buffersize);
+    }
 
     // snaplen, prmisc, read_timeout_ms,
 
     mask = 0;
-    // don't optimize
     ret = pcap_compile(handle, &compiled_filter, filter, 0, mask);
     printf("pcap compile returns %i \n", ret);
     if(ret == -1) {
-        printf("pcap compilefilter failed: %s\n", pcap_geterr(handle));
+        fprintf(stderr, "pcap compilefilter failed: %s\n", pcap_geterr(handle));
         exit(1);
     }
 
     ret = pcap_setfilter(handle, &compiled_filter);
     if(ret == -1) {
-        printf("pcap setfilter failed: %s", errbuf);
+        fprintf(stderr, "pcap setfilter failed: %s", errbuf);
         exit(1);
     }
     ret = pcap_activate(handle);
     if(ret < 0) {
-        printf("pcap activate failed: %s\n", pcap_geterr(handle));
+        fprintf(stderr, "pcap activate failed: %s\n", pcap_geterr(handle));
         exit(1);
     }
-
-
     file_handle = pcap_get_selectable_fd(handle);
     if(file_handle < 0) {
-        printf("pcap can not get selectable filehandler");
+        fprintf(stderr, "pcap can not get selectable filehandler");
         exit(1);
     }
 
-    init_udp(ip, port);
 
     int epollfd, nfds, n;
 
     epollfd = epoll_create(MAX_EVENTS);
     if(epollfd < 0) {
         perror("while epoll ");
-        printf("Can not setup epoll");
+        fprintf(stderr, "Can not setup epoll");
         exit(6);
     }
+
+    init_udp(ip, port);
+
     struct epoll_event ev;
     ev.events = EPOLLIN;
     ev.data.fd = file_handle;
     ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, file_handle, &ev);
     if (ret < 0) {
-        printf("error while adding eloop");
+        fprintf(stderr, "error while adding eloop");
         exit(1);
     }
-
 
     while(1) {
         nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
         printf("socket nfds: %i\n", nfds);
-        for (n = 0; n < nfds; ++n) {
-            if (events[n].data.fd == file_handle)
+
+        for(n = 0; n < nfds; ++n) {
+            if(events[n].data.fd == file_handle) {
                 handle_pcap(handle);
-            else
+            } else if(n == EINTR) {
+                fprintf(stderr, "SIGNAL caugth\n");
+                exit(1);
+            }
+            else {
                 printf("unknown filehandle\n");
+            }
         }
     }
 }
--- src/udp.c
+++ src/udp.c
@@ -5,45 +5,77 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
 
 #include <pcap.h>
 
-static struct sockaddr_in servaddr;
+// 30 seconds
+#define RETRY_INTERVAL 30
+
+static struct sockaddr_in remote_addr;
 static int sockfd = -1;
+static time_t reconnect_timeout = 0;
 
 #define HEADER_SIZE 16
 #define MAX_PACKET_SIZE 4096
 
-int init_udp(char *ip, int port) {
-    memset(&servaddr, 0, sizeof(struct sockaddr_in));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr= inet_addr(ip);
-    servaddr.sin_port =  htons(port);
-
+int reconnect_udp() {
+    if(reconnect_timeout > time(NULL)) {
+        return -EBUSY;
+    }
+    reconnect_timeout = time(NULL) + RETRY_INTERVAL;
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr_in));
 
     return sockfd;
 }
 
+int init_udp(char *ip, int port) {
+    memset(&remote_addr, 0, sizeof(struct sockaddr_in));
+
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_addr.s_addr= inet_addr(ip);
+    remote_addr.sin_port =  htons(port);
+
+    return reconnect_udp();
+}
 
 int send_udp(struct pcap_pkthdr *pkghdr, u_char *rawpacket) {
     char netpkg[MAX_PACKET_SIZE];
     int netpkg_size = 0;
-    if(sockfd < 0)
-        return sockfd;
+    int ret;
+    if(sockfd < 0) {
+        ret = reconnect_udp();
+        if(ret < 0) {
+            return ret;
+        }
+    }
+    if(rawpacket == NULL) {
+        fprintf(stderr, "error rawpacket is null");
+        return -1;
+    } else if(pkghdr == NULL) {
+        fprintf(stderr, "error pkghdr is null");
+        return -1;
+    }
 
-    if((pkghdr->len + HEADER_SIZE) > MAX_PACKET_SIZE) {
+    if((pkghdr->caplen + HEADER_SIZE) > MAX_PACKET_SIZE) {
         netpkg_size = MAX_PACKET_SIZE - HEADER_SIZE;
     } else {
-        netpkg_size = pkghdr->len;
+        netpkg_size = pkghdr->caplen + HEADER_SIZE;
     }
 
     // clear header
     memset(netpkg, 0, HEADER_SIZE);
     memcpy((netpkg + HEADER_SIZE + sizeof(struct pcap_pkthdr)), pkghdr, sizeof(struct pcap_pkthdr));
     // copy raw packet
-    memcpy((netpkg + HEADER_SIZE + sizeof(struct pcap_pkthdr)), rawpacket, netpkg_size);
+    memcpy((netpkg + HEADER_SIZE + sizeof(struct pcap_pkthdr)), rawpacket, netpkg_size - (HEADER_SIZE + sizeof(struct pcap_pkthdr)) );
     // save 64 or 32 bit stuff
     strncpy(netpkg, "UDP2PCAP", HEADER_SIZE);
-    return sendto(sockfd, netpkg, netpkg_size, 0,(struct sockaddr *) &servaddr, sizeof(struct sockaddr_in));
+    ret = send(sockfd, netpkg, netpkg_size, 0);
+    if(ret < 0) {
+
+    }
+
+    return ret;
 }
